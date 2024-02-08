756819c7435cecac107ccbbcac9072c8376adfa9
Abdallah Elsayed <abdallahae15@gmail.com>
2024-02-07 21:54:51 -0500

    obj loading

--- mnlt/model.cpp
+++ mnlt/model.cpp
@@ -1,19 +1,51 @@
 #include "model.hpp"
+#include "utils.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "../libs/tinyobjloader/tiny_obj_loader.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 #include <cassert>
 #include <cstring>
+#include <unordered_map>
+
+namespace std {
+template <>
+struct hash<mnlt::Model::Vertex> {
+  size_t operator()(mnlt::Model::Vertex const &vertex) const {
+    size_t seed = 0;
+    mnlt::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+    return seed;
+  }
+};
+}
 
 namespace mnlt
 {
-    Model::Model(Device &device, const std::vector<Vertex> &vertices) : device{device} 
+    Model::Model(Device &device, const Model::BuilderData &builderData) : device{device} 
     {
-        createVertexBuffers(vertices);
+        createVertexBuffers(builderData.vertices);
+        createIndexBuffer(builderData.indices);
     }
 
     Model::~Model() 
     {
         vkDestroyBuffer(device.device(), vertexBuffer, nullptr);
         vkFreeMemory(device.device(), vertexBufferMemory, nullptr);
+
+        if (hasIndexBuffer) 
+        {
+            vkDestroyBuffer(device.device(), indexBuffer, nullptr);
+            vkFreeMemory(device.device(), indexBufferMemory, nullptr);
+        }
+    }
+
+    std::unique_ptr<Model> Model::createModelFromFile(Device &device, const std::string &filepath) 
+    {
+        BuilderData builderData{};
+        builderData.loadModel(filepath);
+        return std::make_unique<Model>(device, builderData);
     }
 
     void Model::createVertexBuffers(const std::vector<Vertex> &vertices) 
@@ -21,22 +53,88 @@ namespace mnlt
         vertexCount = static_cast<uint32_t>(vertices.size());
         assert(vertexCount >= 3 && "Vertex count must be at least 3");
         VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
-        device.createBuffer(
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+        device.createBuffer
+        (
             bufferSize,
-            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-            vertexBuffer,
-            vertexBufferMemory);
+            stagingBuffer,
+            stagingBufferMemory
+        );
 
         void *data;
-        vkMapMemory(device.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
+        vkMapMemory(device.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
         memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-        vkUnmapMemory(device.device(), vertexBufferMemory);
+        vkUnmapMemory(device.device(), stagingBufferMemory);
+
+        device.createBuffer
+        (
+            bufferSize,
+            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+            vertexBuffer,
+            vertexBufferMemory
+            );
+
+        device.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+        vkDestroyBuffer(device.device(), stagingBuffer, nullptr);
+        vkFreeMemory(device.device(), stagingBufferMemory, nullptr);
+    }
+
+    void Model::createIndexBuffer(const std::vector<uint32_t> &indices) 
+    {
+        indexCount = static_cast<uint32_t>(indices.size());
+        hasIndexBuffer = indexCount > 0;
+
+        if (!hasIndexBuffer) { return; }
+
+        VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+        device.createBuffer
+        (
+            bufferSize,
+            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            stagingBuffer,
+            stagingBufferMemory
+        );
+
+        void *data;
+        vkMapMemory(device.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(device.device(), stagingBufferMemory);
+
+        device.createBuffer
+        (
+            bufferSize,
+            VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+            indexBuffer,
+            indexBufferMemory
+        );
+
+        device.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+        vkDestroyBuffer(device.device(), stagingBuffer, nullptr);
+        vkFreeMemory(device.device(), stagingBufferMemory, nullptr);
     }
 
     void Model::draw(VkCommandBuffer commandBuffer) 
     {
-        vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+        if (hasIndexBuffer)
+        {
+            vkCmdDrawIndexed(commandBuffer, indexCount, 1, 0, 0, 0);
+        } 
+        else
+        {
+            vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+        }
     }
 
     void Model::bind(VkCommandBuffer commandBuffer) 
@@ -44,6 +142,11 @@ namespace mnlt
         VkBuffer buffers[] = {vertexBuffer};
         VkDeviceSize offsets[] = {0};
         vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+        if (hasIndexBuffer) 
+        {
+            vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+        }
     }
 
     std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptions() 
@@ -72,4 +175,73 @@ namespace mnlt
 
         return attributeDescriptions;
     }
+
+    void Model::BuilderData::loadModel(const std::string &filepath) 
+    {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
+
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) 
+        {
+            throw std::runtime_error(warn + err);
+        }
+
+        vertices.clear();
+        indices.clear();
+
+        std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+        for (const auto &shape : shapes) 
+        {
+            for (const auto &index : shape.mesh.indices) 
+            {
+                Vertex vertex{};
+
+                if (index.vertex_index >= 0) 
+                {
+                    vertex.position = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2],
+                    };
+
+                    auto colorIndex = 3 * index.vertex_index + 2;
+                    if (colorIndex < attrib.colors.size()) {
+                    vertex.color = {
+                        attrib.colors[colorIndex - 2],
+                        attrib.colors[colorIndex - 1],
+                        attrib.colors[colorIndex - 0],
+                    };
+                    } else {
+                    vertex.color = {1.f, 1.f, 1.f};  // set default color
+                    }
+                }
+
+                if (index.normal_index >= 0) 
+                {
+                    vertex.normal = {
+                        attrib.normals[3 * index.normal_index + 0],
+                        attrib.normals[3 * index.normal_index + 1],
+                        attrib.normals[3 * index.normal_index + 2],
+                    };
+                }
+
+                if (index.texcoord_index >= 0) 
+                {
+                    vertex.uv = {
+                        attrib.texcoords[2 * index.texcoord_index + 0],
+                        attrib.texcoords[2 * index.texcoord_index + 1],
+                    };
+                }
+
+                if (uniqueVertices.count(vertex) == 0) 
+                {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices[vertex]);
+            }
+        }
+        }
 }
